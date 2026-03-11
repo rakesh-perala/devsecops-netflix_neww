d216b9134113ae63399bf4434042cf79a5741bf8
Rakesh Perala <raakeshperala16@gmail.com>
2026-03-12 00:02:36 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "peralarakeshbucket" # Replace with your actual S3 bucket name
+    bucket = "terraform-buketrk" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
